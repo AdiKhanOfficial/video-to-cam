2668eb9012d073de8f622cff86ab4b2e09d88023
Adil Khan <AdiKhanOfficial@gmail.com>
2024-12-16 21:40:26 +0500

    Create Install.bat

---
+++ Drivers/Install.bat
@@ -0,0 +1,20 @@
+@echo off
+
+:-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Please Run as Administrator..
+	pause
+	exit /B
+)else ( goto gotAdmin )
+
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+    regsvr32 "UnityCaptureFilter32.dll"
+    regsvr32 "UnityCaptureFilter64.dll"
+:--------------------------------------
